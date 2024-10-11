36c952050638fee617342c0d7f80a1b77ee0496d
AyushmanPriyadarshan <git@stackblitz.com>
2024-10-11 06:17:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const { resolve } = require('path');
+let cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+/**Server- side values */
+let taxRate = 5; // 5%
+let discountPercentage = 10; // 10%
+let loyaltyRate = 2; // 2 points per $1
+
+const port = 3000;
+
+app.get('/cart-total', (req, res) => {
+  let newItemPrice = parseFloat(req.query.newItemPrice);
+  let cartTotal = parseFloat(req.query.cartTotal);
+
+  let totalCartValue = newItemPrice + cartTotal;
+  res.send(totalCartValue.toString());
+});
+
+app.get('/membership-discount', (req, res) => {
+  let isMember = req.query.isMember === 'true';
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let finalPrice;
+
+  if (isMember) {
+    let discountedAmount = (cartTotal * discountPercentage) / 100;
+    finalPrice = cartTotal - discountedAmount;
+  } else {
+    finalPrice = cartTotal;
+  }
+
+  res.send(finalPrice.toString());
+});
+
+app.get('/calculate-tax', (req, res) => {
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let taxableAmount;
+
+  taxableAmount = (cartTotal * taxRate) / 100;
+
+  res.send(taxableAmount.toString());
+});
+
+app.get('/estimate-delivery', (req, res) => {
+  let shippingMethod = req.query.shippingMethod;
+  let distance = parseFloat(req.query.distance);
+
+  let requiredDeliveryDays;
+  if (shippingMethod === 'Standard') {
+    requiredDeliveryDays = Math.ceil(distance / 50);
+    res.send(requiredDeliveryDays.toString());
+  } else if (shippingMethod === 'Express') {
+    requiredDeliveryDays = Math.ceil(distance / 100);
+    res.send(requiredDeliveryDays.toString());
+  } else {
+    res.send(`Please select the shipping method`);
+  }
+});
+
+app.get('/shipping-cost', (req, res) => {
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
+
+  let baseWeight = 2;
+  let shippingCost = weight * distance * 0.1;
+
+  if (weight <= baseWeight) {
+    shippingCost = weight * distance * 0.1;
+    res.send(shippingCost.toString());
+  } else {
+    let additionalWeight = weight - baseWeight;
+    let additionalShippingCost = additionalWeight * distance * 0.1;
+    let totalShippingCost = shippingCost + additionalShippingCost;
+    res.send(totalShippingCost.toString());
+  }
+});
+
+app.get('/loyalty-points', (req, res) => {
+  let purchaseAmount = parseFloat(req.query.purchaseAmount);
+
+  let loyaltyPoints = Math.floor(purchaseAmount * loyaltyRate);
+  res.send(loyaltyPoints.toString());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
